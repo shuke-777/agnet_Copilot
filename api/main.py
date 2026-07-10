a28be6bec8f3ac593d4a5dbad87cc9790db34f55
舒克卷不动啦 <976347841@qq.com>
2026-07-10 23:13:57 +0800

    feishu_finish

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from api.agent_trace import router as agent_trace_router
+from api.admin import router as admin_router
 from api.business import router as business_router
 from api.copilot import router as copilot_router
 from services.bootstrap import bootstrap_database
@@ -19,6 +20,7 @@ app = FastAPI(
 
 bootstrap_database()
 app.include_router(agent_trace_router)
+app.include_router(admin_router)
 app.include_router(business_router)
 app.include_router(copilot_router)
 
